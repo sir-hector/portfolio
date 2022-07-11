c6b1a7e552c9a0a22f87f3ab776af54acac40045
sir-hector <krauskarol7@gmail.com>
2022-07-11 19:05:01 +0200

    Header Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className='container header__container'>
+      <h5>Hello I'm</h5>
+      <h1>Karol Kraus</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      <CTA/>
+
+      <div className="me">
+        <img src={ME} alt="me" />
+      </div>
+
+      <a href="#contact" className='scroll__down'>Scrool Down</a>
+    </div>
   )
 }
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://linkedin.com" target="_blank"></a>
+        <a href="https://github.com" target="_blank"></a>
+        <a href="https://instagram.com" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
