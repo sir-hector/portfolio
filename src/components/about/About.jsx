aa3c8f80056999fc234f08057104e38a93b8f649
sir-hector <krauskarol7@gmail.com>
2022-07-11 22:19:48 +0200

    add about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 import './about.css'
+import image from '../../assets/me.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 export const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <img src={image} className="about__me-image" />
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ year working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>5+ clients</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>5+ complete projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, molestias asperiores! Consectetur, veritatis velit? Animi ab totam iusto ex provident autem quibusdam repellendus quam amet. Et laborum excepturi tempora expedita.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
