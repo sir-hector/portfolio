199a0ccce97dbe8dc9db66086eeae6b721c9b4d5
sir-hector <krauskarol7@gmail.com>
2022-07-16 17:08:21 +0200

    add portfolio section with styling

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
+import './portfolio.css'
+import ME from '../../assets/me.jpg'
 
 export const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={ME} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={ME} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={ME} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={ME} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+  
+
+      </div>
+    </section>
   )
 }
