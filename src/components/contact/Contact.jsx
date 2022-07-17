a110ffc8847ad2dd69ff07a2c9cac69bd86883e9
sir-hector <krauskarol7@gmail.com>
2022-07-17 14:01:04 +0200

    form submission using emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_uik67je', 'template_7mofz55', form.current, '4Yy_22ij8v-VUovWV')
+    e.target.reset();
+     
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -31,7 +40,7 @@ export const Contact = () => {
             <a href="https://api.whatsapp.com/send?phone=+48791773420" target="_blank">Send a message</a>
           </article>
         </div>
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full name' required/>
           <input type="email" name='email' placeholder='Your e-mail' required/>
           <textarea name="message" id=""rows="7" placeholder='Your Message' required></textarea>
